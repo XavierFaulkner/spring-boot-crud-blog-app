c3768791692fcfa07a227104257406c7050c3888
XavierFaulkner <101128878+XavierFaulkner@users.noreply.github.com>
2022-06-08 14:53:25 -0400

    spring boot init

---
+++ src/main/java/com/ctac/springboot/CrudBlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.ctac.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBlogAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ctac/springboot/CrudBlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctac.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
