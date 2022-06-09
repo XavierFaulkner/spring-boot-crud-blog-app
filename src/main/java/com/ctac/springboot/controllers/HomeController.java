203f40195279f1d78090f76048e183ba46ff4e25
XavierFaulkner <xjfaulkner@gmail.com>
2022-06-09 01:57:06 -0400

    Completed file structure

--- src/main/java/com/ctac/springboot/controllers/HomeController.java
+++ src/main/java/com/ctac/springboot/controllers/HomeController.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
     @GetMapping("/")
     public String home() {
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
