36612ac5814f02a1ad5011c7291f26ea22df5b66
XavierFaulkner <101128878+XavierFaulkner@users.noreply.github.com>
2022-06-14 12:31:26 -0400

    functional create post

--- src/main/java/com/ctac/springboot/services/UserService.java
+++ src/main/java/com/ctac/springboot/services/UserService.java
@@ -11,4 +11,6 @@ public interface UserService extends UserDetailsService {
     User save(UserRegistrationDto registrationDto);
 
     List<User> getAllUsers();
+
+    User getUserByUsername(String username);
 }
--- src/main/java/com/ctac/springboot/services/UserServiceImpl.java
+++ src/main/java/com/ctac/springboot/services/UserServiceImpl.java
@@ -48,6 +48,16 @@ public class UserServiceImpl implements UserService{
         }
         return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
     }
+
+    @Override
+    public User getUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if(user == null) {
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+        return user;
+    }
+
     private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles){
         return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
     }
